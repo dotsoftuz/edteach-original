86093f5f6791f975b14fbf6a0210e58307d7ad14
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-11-17 23:19:33 +0500

    build: navbar, header, footer, about, signup

---
+++ components/about/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="container mx-auto grid grid-cols-3 mb-10">
+      <div class="space-y-1 text-center">
+        <img class="mx-auto sm:w-1/4" src="https://kutty.netlify.app/empty.png" />
+        <h2 class="text-lg font-medium text-gray-800">
+          All tasks are completed!
+        </h2>
+        <p class="text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maxime
+          maiores consectetur necessitatibus animi ea veniam optio eos! Id animi
+          et excepturi earum aliquid deleniti.
+        </p>
+      </div>
+      <div class="space-y-1 text-center">
+        <img class="mx-auto sm:w-1/4" src="https://kutty.netlify.app/empty.png" />
+        <h2 class="text-lg font-medium text-gray-800">
+          All tasks are completed!
+        </h2>
+        <p class="text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maxime
+          maiores consectetur necessitatibus animi ea veniam optio eos! Id animi
+          et excepturi earum aliquid deleniti.
+        </p>
+      </div>
+      <div class="space-y-1 text-center">
+        <img class="mx-auto sm:w-1/4" src="https://kutty.netlify.app/empty.png" />
+        <h2 class="text-lg font-medium text-gray-800">
+          All tasks are completed!
+        </h2>
+        <p class="text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maxime
+          maiores consectetur necessitatibus animi ea veniam optio eos! Id animi
+          et excepturi earum aliquid deleniti.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ components/index.js
@@ -0,0 +1,9 @@
+// Layout
+export { default as Layout } from "./layout/index";
+export { default as Navbar } from "./navbar/index";
+export { default as Footer } from "./footer/index";
+
+// Sections
+export { default as Header } from "./header/index";
+export { default as About } from "./about/index";
+export { default as Logos } from "./logos/index";
