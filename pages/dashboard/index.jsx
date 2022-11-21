13b681c6877ada1eb4017b1f3d2cec477b703128
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-11-21 11:51:12 +0500

    fix: push

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Footer, Navbar } from '../../components';
-import Loader from '../../components/Loader/Loader';
-import { useUserContext } from '../../context/userContext';
-import App from '../App';
-import Registration from '../registration';
-
+import React from "react";
+import { Footer, Navbar } from "../../components";
+import Loader from "../../components/Loader/Loader";
+import { useUserContext } from "../../context/userContext";
+import App from "../App";
+import Registration from "../registration";
 
 function Dashboard(props) {
-    const { user, loading } = useUserContext();
-    if (loading) {
-        return <Loader />
-    }
-    return (
-        <div>
-
-            {loading ? <Loader /> : <> {user ? <App /> : <Registration />}</>}
-        </div>
-    );
+  const { user, loading } = useUserContext();
+  if (loading) {
+    return <Loader />;
+  }
+  return (
+    <div>
+      {loading ? <Loader /> : <> {user ? <App /> : <Registration />}</>}
+    </div>
+  );
 }
 
 export default Dashboard;
