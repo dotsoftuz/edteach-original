d06def59cda212860a35ced75853f724b6a73b2d
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-11-21 11:59:03 +0500

    ready to coding

--- app/layout/index.jsx
+++ app/layout/index.jsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD:pages/registration/index.jsx
 import React from "react";
 import { Footer, Navbar } from "../../components";
 import { useUserContext } from "../../context/userContext";
-import SignIn from "./signin";
-import SignUp from "./signup";
-=======
-import React from 'react';
-import { Footer, Navbar } from '../../components';
-import { useUserContext } from '../../context/userContext';
-import SignIn from '../../components';
-import SignUp from '../../components';
->>>>>>> fec7a23407ab79306ff41b6f6dee99d54316d345:app/layout/index.jsx
+import SignIn from "../../components";
+import SignUp from "../../components";
 
 function Registration(props) {
   const { registry } = useUserContext();
--- components/index.js
+++ components/index.js
@@ -1,11 +1,14 @@
 // Layout
 export { default as Navbar } from "./navbar/index";
 export { default as Footer } from "./footer/index";
+
+// Loader
 export { default as Loader } from "./loader/index";
+
+// Register
 export { default as SignIn } from "./signin/index";
 export { default as SignUp } from "./signup/index";
 
 // Sections
 export { default as Header } from "./header/index";
 export { default as About } from "./about/index";
-export { default as Logos } from "./logos/index";
