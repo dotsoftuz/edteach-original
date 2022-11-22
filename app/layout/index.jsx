0d23868afa8ed85be9c7d4169d63be53b1def998
Muzaffar <muzaffarsafarov880@gmail.com>
2022-11-22 12:36:33 +0500

    fix: sidebar, tests

--- app/layout/index.jsx
+++ app/layout/index.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { Footer, Navbar } from "../../components";
 import { useUserContext } from "../../context/userContext";
-<<<<<<< HEAD
 import { SignIn, SignUp } from "../../components";
-=======
 
-import {SignIn, SignUp} from "../../components";
->>>>>>> 91bf1e34f1400048182c2bee0a49847ff06e154b
 
 export function Layout() {
   const { registry } = useUserContext();
