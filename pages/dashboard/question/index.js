0bb0e885b177a55977c916040341be8a87473dac
Storm First <libertywalk5555@gmail.com>
2022-11-24 14:58:05 +0500

    startPage added

--- pages/dashboard/question/index.js
+++ pages/dashboard/question/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import Head from 'next/head';
 import Link from 'next/link';
 import { db } from '../../../firebase';
@@ -22,10 +22,11 @@ const Tests = () => {
   }, [uid]);
 
   const sendData = async (id) => {
-    const collectionRef = doc(db, `question`, id);
+    const collectionRef = doc(db, `users/${uid}/question`, id);
     const payload = {
       status: "started",
-      pin: String(Math.floor(Math.random() * 9000) + 1000),
+      pin: String(Math.floor(Math.random() * 900000) + 1000),
+      id: id
     };
     await updateDoc(collectionRef, payload);
   };
@@ -129,70 +130,72 @@ const Tests = () => {
             .map((val, key) => {
               return (
                 
-                  <Link href={`/dashboard/question/${val.id}`} >
-                    <div
-                      className={`${
-                        testCard
-                          ? 'flex flex-col space-y-2'
-                          : 'flex flex-row space-x-2 md:space-x-4'
-                      } relative  p-4 rounded-lg bg-gray-200`}
-                    >
-                      <Link href={`/dashboard/question/${val.id}`}>
-                        <img
-                          className={`${
-                            testCard ? 'w-full' : 'w-72'
-                          } rounded-lg h-56 object-cover cursor-pointer`}
-                          src="/images/about-img1.jpg"
-                          alt="test image"
-                        />
-                      </Link>
-                      <div className="flex flex-col justify-between">
-                        <div>
-                          <span className="bg-purple-500 text-white text-xs font-semibold px-1 rounded-full">
-                            Quiz
-                          </span>
-                          <h2 className="text-xl font-semibold">{val.title}</h2>
-                          <h2 className="text-lg font-semibold">
-                            {val.description}
-                          </h2>
-                        </div>
-                        <div>
-                          <div className="flex items-center space-x-2">
-                            <h2 className=" text-base font-semibold">
-                              Umumiy testlar soni:
-                            </h2>
-                            <span className="bg-purple-500 text-white text-xs font-semibold px-1 rounded-full">
-                              {val.questionList.length} ta
-                            </span>
-                          </div>
-                          <h2 className="text-xs font-semibold">
-                            Yaratuvchi: Zebiniso
-                          </h2>
-                        </div>
-                      </div>
-                      <div
+                  <div
+                    className={`${
+                      testCard
+                        ? 'flex flex-col space-y-2'
+                        : 'flex flex-row space-x-2 md:space-x-4'
+                    } relative  p-4 rounded-lg bg-gray-200`}
+                  >
+                    <Link href={`/dashboard/question/${val.id}`}>
+                      <img
                         className={`${
-                          testCard ? 'bottom-2' : 'top-2'
-                        } absolute  right-2 flex items-center space-x-1 hover:text-purple-500 cursor-pointer`}
-                      >
-                        <p className="text-lg font-semibold">Start</p>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-5 h-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-                          />
-                        </svg>
+                          testCard ? 'w-full' : 'w-72'
+                        } rounded-lg h-56 object-cover cursor-pointer`}
+                        src="/images/about-img1.jpg"
+                        alt="test image"
+                      />
+                    </Link>
+                    <div className="flex flex-col justify-between">
+                      <div>
+                        <span className="bg-purple-500 text-white text-xs font-semibold px-1 rounded-full">
+                          Quiz
+                        </span>
+                        <h2 className="text-xl font-semibold">{val.title}</h2>
+                        <h2 className="text-lg font-semibold">
+                          {val.description}
+                        </h2>
+                      </div>
+                      <div>
+                        <div className="flex items-center space-x-2">
+                          <h2 className=" text-base font-semibold">
+                            Umumiy testlar soni:
+                          </h2>
+                          <span className="bg-purple-500 text-white text-xs font-semibold px-1 rounded-full">
+                            {val.questionList.length} ta
+                          </span>
+                        </div>
+                        <h2 className="text-xs font-semibold">
+                          Yaratuvchi: Zebiniso
+                        </h2>
                       </div>
                     </div>
-                  </Link>
+                    <div onClick={() => sendData(val.id)}>
+                      <Link href={`startgame/${val.id}`}>
+                        <div
+                          className={`${
+                            testCard ? 'bottom-2' : 'top-2'
+                          } absolute  right-2 flex items-center space-x-1 hover:text-purple-500 cursor-pointer`}
+                        >
+                          <p className="text-lg font-semibold">Start</p>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-5 h-5"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+                            />
+                          </svg>
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
                 );
               })}
         </div>
