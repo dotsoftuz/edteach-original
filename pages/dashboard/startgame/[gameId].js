886c768ca62c0900b25a5ea65eb873610881a440
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-11-25 10:52:13 +0500

    build: start game

--- pages/dashboard/startgame/[gameId].js
+++ pages/dashboard/startgame/[gameId].js
@@ -26,11 +26,40 @@ function GameID() {
 
   return (
     <Sidebar>
-      <h1>
-        {questions.map((item) => {
-          return item.pin;
+      <div className="flex items-center justify-center h-screen">
+        {questions.map((game) => {
+          console.log(game);
+          return (
+            <div className="w-[100%] md:[90%] xl:w-[40%] bg-gray-200 p-4 md:p-8 rounded-2xl">
+              <h1 className="text-center text-xl font-semibold uppercase">
+                Kutish rejimida
+              </h1>
+              <h2 className="text-center text-lg my-2 font-semibold">
+                PIN <span className='text-purple-500'>{game.pin}</span>
+              </h2>
+              <div className="w-full bg-gray-100 h-[300px] md:h-[350px] p-4 text-center text-base font-semibold rounded-2xl">
+                <div className="relative overflow-y-scroll overflow-x-hidden scrollbar-hide h-full block w-full ">
+                  <h1 className="absolute top-0 left-0 right-0">
+                    Ishtorkchilar ro`yhati
+                  </h1>
+                  <div className="p-0 md:p-5 h-full">
+                    <ol>
+                      <li className="flex items-center justify-between bg-gray-200 px-4 py-3 my-2 rounded-lg">
+                        <p className="text-lg font-semibold">1</p>
+                        <h1 className="ml-0 xl:ml-5">Jo`shqin</h1>
+                        <small>Talaba</small>
+                      </li>
+                    </ol>
+                    <h1 className="flex items-center justify-center h-full -mt-5 text-red-500 font-semibold text-lg">
+                      Hali ishtorkchilar yo`q
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
         })}
-      </h1>
+      </div>
     </Sidebar>
   );
 }
