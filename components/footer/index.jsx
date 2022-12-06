df0086c34b08e7b115df552fa286f77ac5af963d
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-12-06 11:52:56 +0500

    build: change footer design

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,107 +1,190 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="container mx-auto px-5 pt-16 lg:pt-24  lg:max-w-screen-xl md:px-24">
-      <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
-        <div className="sm:col-span-2">
-          <Link href="/" ariaLabel="Go Bosh sahifa" title="Company">
-            <a className="inline-flex items-center">
-              {/* logo qo&apos;yilishi mumkin */}
-              <span className=" text-xl font-bold tracking-wide text-gray-800 uppercase">
-                Ed Teach
-              </span>
+    <footer className="container mx-auto pt-24 bg-blue-50">
+      <div className="">
+        <div className="pb-10 border-b">
+          <div className="flex items-center justify-between">
+            <div className="w-full sm:w-1/2 lg:w-2/12 p-8">
+              <h3 className="mb-6 font-semibold leading-normal">Company</h3>
+              <ul>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Careers
+                  </a>
+                </li>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Press
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Blog
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-2/12 p-8">
+              <h3 className="mb-6 font-semibold leading-normal">Pages</h3>
+              <ul>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Login
+                  </a>
+                </li>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Register
+                  </a>
+                </li>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Add list
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-2/12 p-8">
+              <h3 className="mb-6 font-semibold leading-normal">Legal</h3>
+              <ul>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Terms
+                  </a>
+                </li>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li className="mb-3.5">
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Team
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-gray-600 hover:text-gray-700 font-medium leading-relaxed"
+                    href="#"
+                  >
+                    Privacy
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-4/12 p-8">
+              <div className="lg:max-w-sm">
+                <h3 className="mb-6 font-semibold leading-normal">
+                  Obuna bo&apos;ling.
+                </h3>
+                <p className="mb-5 font-sans text-gray-600 leading-relaxed">
+                  Qo&apos;shimcha yangilaklardan habardor bo&apos;lib turish uchun obuna bo&apos;ling.
+                </p>
+                <div className="mb-3 xl:pl-6 inline-block md:max-w-xl w-full overflow-hidden border border-gray-300 rounded-xl focus-within:ring focus-within:ring-indigo-300">
+                  <div className="flex flex-wrap items-center">
+                    <div className="w-full xl:flex-1">
+                      <input
+                        className="p-2 xl:p-0 xl:pr-6 w-full font-medium bg-transparent text-gray-500 placeholder-gray-500 outline-none"
+                        id="footerInput1-1"
+                        type="text"
+                        placeholder="Emailni kiriting."
+                      />
+                    </div>
+                    <div className="w-full xl:w-auto">
+                      <div className="block">
+                        <button
+                          className="py-3 px-4 w-full text-white font-semibold border border-blue-700 focus:ring focus:ring-blue-300 bg-blue-600 hover:bg-blue-700 transition ease-in-out duration-200"
+                          type="button"
+                        >
+                          Obuna bo&apos;lish
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center py-6">
+          <Link href="/">
+            <a classNameName="cursor-pointer">
+              <Image
+                width={110}
+                height={40}
+                src="/images/logo.svg"
+                alt="logo"
+              />
             </a>
           </Link>
-          <div className="mt-6 lg:max-w-sm">
-            <p className="text-sm text-gray-800">
-              Jizzax viloyati Jizzax shahri
-            </p>
-            <p className="mt-4 text-sm text-gray-800">
-              Sh.Rashidov shox ko&apos;chasi, 5-uy
-            </p>
-          </div>
-        </div>
-        <div className="space-y-2 text-sm">
-          <p className="text-base font-bold tracking-wide text-gray-900">
-          Kontaktlar
-          </p>
-          <div className="flex">
-            <p className="mr-1 text-gray-800">Phone:</p>
-            <Link
-              href="tel:850-123-5021"
-              ariaLabel="Our phone"
-              title="Our phone"
-              className="transition-colors duration-300 text-deep-blue-accent-400 hover:text-deep-blue-800"
-            >
-              850-123-5021
-            </Link>
-          </div>
-          <div className="flex">
-            <p className="mr-1 text-gray-800">Email:</p>
-            <Link
-              href="mailto:info@lorem.mail"
-              ariaLabel="Our email"
-              title="Our email"
-              className="transition-colors duration-300 text-deep-blue-accent-400 hover:text-deep-blue-800"
-            >
-              info@lorem.mail
-            </Link>
-          </div>
-          <div className="flex">
-            <p className="mr-1 text-gray-800">Address:</p>
-            <Link
-              href="https://www.google.com/maps"
+          <div className="w-auto p-4 text-sm md:text-base text-gray-600 font-medium">
+            <a
+              href="https://dotsoft.uz"
               target="_blank"
-              rel="noopener noreferrer"
-              ariaLabel="Our address"
-              title="Our address"
-              className="transition-colors duration-300 text-deep-blue-accent-400 hover:text-deep-blue-800"
+              className="mr-1 text-blue-600 hover:underline"
             >
-              312 Lovely Street, NY
-            </Link>
+              Dotsoft team.
+            </a>
+            &copy;2022
+          </div>
+          <div className="w-auto p-4">
+            <div className="flex items-center justify-between">
+              <div className="w-auto p-4">Icons</div>
+              <div className="w-auto p-4">Icons</div>
+              <div className="w-auto p-4">Icons</div>
+              <div className="w-auto p-4">Icons</div>
+            </div>
           </div>
         </div>
-        <div>
-          <span className="text-base font-bold tracking-wide text-gray-900">
-            Ijtimoiy Tarmoqlarimiz
-          </span>
-          <div className="flex items-center mt-1 space-x-3">
-            <Link href="/">
-              <a className="text-gray-500 transition-colors duration-300 hover:text-deep-blue-accent-400">
-                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                  <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
-                </svg>
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="text-gray-500 transition-colors duration-300 hover:text-deep-blue-accent-400">
-                <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
-                  <circle cx="15" cy="15" r="4" />
-                  <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
-                </svg>
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="text-gray-500 transition-colors duration-300 hover:text-deep-blue-accent-400">
-                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                  <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
-                </svg>
-              </a>
-            </Link>
-          </div>
-          <p className="mt-4 text-sm text-gray-500">
-            Bizning rasmiy ijtimoiy kanallarimizga obuna bo&apos;ling
-          </p>
-        </div>
       </div>
-      <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
-        <p className="text-sm text-gray-600">
-          © anonymous 2022 Lorem Inc. All rights reserved.
-        </p>
-      </div>
-    </div>
+    </footer>
   );
 };
 
